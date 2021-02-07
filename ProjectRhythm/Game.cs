fbb2f532b013e0fcbe0d59327f1db24ce6fdf1a4
Brandon Ault <rbrandonault@gmail.com>
2021-02-07 17:19:19 -0600

    Creating repo, renaming files.

--- ProjectRhythm/Game.cs
+++ ProjectRhythm/Game.cs
@@ -2,17 +2,17 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace MonoGameWindowsStarter
+namespace ProjectRhythm
 {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class Game : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public Game()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
